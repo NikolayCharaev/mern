6db78193b636dfc0e92d5942d2f8617407e78d33
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-05-11 07:57:25 +0300

    верстка компонента Home

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -1,10 +1,29 @@
 import { useEffect } from 'react';
-import axios from '../../axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '../../redux/slices/posts';
+import { FaSpinner } from 'react-icons/fa';
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const { posts } = useSelector((state) => state.posts);
+  const status = useSelector((state) => state.posts.status);
+
   useEffect(() => {
-    axios.get('/posts').then((data) => console.log(data));
+    setTimeout(() => {
+      dispatch(fetchPosts());
+    }, 5000);
   }, []);
-  return <div>Home</div>;
+  return (
+    <>
+      {status === 'loading' && (
+        <div className="w-full h-screen flex items-center flex-col gap-4 justify-center transition">
+            <h1>Идет загрузка</h1>
+          <FaSpinner className="animate-spin w-10 h-10" />
+        </div>
+      )}
+      <div className=""></div>
+    </>
+  );
 };
 
 export default Home;
--- frontend/src/redux/slices/posts.js
+++ frontend/src/redux/slices/posts.js
@@ -1,19 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../axios';
 
-const initialState = { 
-    posts: {
-        items : [],
-        status : 'loading'
-    }
-}
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const { data } = await axios.get('/posts');
+  return data;
+});
+
+const initialState = {
+  posts: [],
+  status: 'loading',
+};
 
 const postSlice = createSlice({
-    name : 'posts',
-    initialState,
-    reducers : { 
+  name: 'posts',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchPosts.pending]: (state) => {
+      state.status = 'loading';
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.status = 'loaded';
+      state.posts = action.payload;
+    },
+    [fetchPosts.rejected]: (state) => {
+      state.status = 'error';
+      state.posts = [];
+    },
+  },
+});
 
-    }
-})
-
-
-export const postsReducer = postSlice.reducer
+export const postsReducer = postSlice.reducer;
